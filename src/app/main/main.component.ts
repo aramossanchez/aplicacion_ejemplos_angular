4973004b9fe9f7ca0def7ba66c1b7bfaed621852
aramossanchez <armandoramossanchez@gmail.com>
2022-06-06 14:25:26 +0200

    uso de {{}}

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-
+  text = '<main>';
+  imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Procyon_lotor_%28Common_raccoon%29.jpg/1200px-Procyon_lotor_%28Common_raccoon%29.jpg';
   constructor() { }
 
   ngOnInit(): void {
